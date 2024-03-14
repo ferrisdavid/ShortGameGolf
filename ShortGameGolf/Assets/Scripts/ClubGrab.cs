8bddd46e5dc35a8e5803f648a4c6401d6edea38a
ferrisdavid <davidferris195@gmail.com>
2024-03-14 02:06:26 -0400

    Added dynamic layer changing for clubs in order to avoid collisions with non ball objects while held to improve experience

--- ShortGameGolf/Assets/Scripts/ClubGrab.cs
+++ ShortGameGolf/Assets/Scripts/ClubGrab.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using Valve.VR;
 
@@ -15,28 +16,40 @@ public class ClubGrab : MonoBehaviour, Grabbable
         // Rotate to have properly oriented club head
         transform.localRotation = Quaternion.LookRotation(hand.transform.up, -hand.transform.forward);
 
-        // Translate Grip to Anchor Point.
+        // // Translate Grip to Anchor Point.
         hand.transform.position = attachPivot.position;
 
-         // Create Fixed Joint for hand
+        // Create Fixed Joint for hand
         FixedJoint joint = hand.AddComponent<FixedJoint>();
         // Assign Break Force and Break Torque Properties for Joint
-        joint.breakForce = Mathf.Infinity;
-        joint.breakTorque = Mathf.Infinity;
+        joint.breakForce = 100000;
+        joint.breakTorque = 100000;
 
         // Assign Object as connected rigidbody
         joint.connectedBody = gameObject.GetComponent<Rigidbody>();
-        joint.anchor = attachPivot.position;
         joint.connectedAnchor = attachPivot.position;
 
         attachedHand = hand;
+
+        gameObject.layer = 6;
+        SetChildLayers(gameObject, 6);
     }
 
     public void OnRelease() {
         // Reset the Connected Body Property of the Controller fixed joint.
         attachedHand.GetComponent<FixedJoint>().connectedBody = null;
-        // Manually Destroy the Fixed Joint
+        // // Manually Destroy the Fixed Joint
         Destroy(attachedHand.GetComponent<FixedJoint>());
+
+        gameObject.layer = 9;
+        SetChildLayers(gameObject, 9);
+    }
+
+    private void SetChildLayers(GameObject obj, int layer) {
+        foreach (Transform child in obj.transform)
+        {
+            child.gameObject.layer = layer;
+        }
     }
     
     // Start is called before the first frame update
