f18621ea08010b45d21bf5d7035f4b3659ef5070
ferrisdavid <davidferris195@gmail.com>
2024-03-15 00:37:20 -0400

    Added put controller script to putter club face to handle the game state updates of contact with the putter and the ball

---
+++ ShortGameGolf/Assets/Scripts/PutController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PutController : MonoBehaviour
+{
+    // High Level Global Game State Object
+    private GameState gameState;
+
+    // Vibration Instance Variables
+    public float minVibrationForce = 0.0f;
+    public float maxVibrationForce = 0.2f;
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("ball")) {
+            // Increment Game State Stroke Count
+            gameState.IncrementStrokes();
+
+            Rigidbody clubRB = gameObject.GetComponentInParent<Rigidbody>();
+            GameObject attachedHand = gameObject.GetComponentInParent<ClubGrab>().GetAttachedHand();
+
+            // Calculate Vibration force based on club velocity
+            float amplitude = Mathf.Clamp((clubRB.velocity.magnitude - 50)/50, minVibrationForce, maxVibrationForce);
+
+            // Perform Haptic Vibration
+            SteamVR_Input_Sources hand = attachedHand.CompareTag("left") ? SteamVR_Input_Sources.LeftHand : SteamVR_Input_Sources.RightHand;
+            SteamVR_Actions.default_Haptic[hand].Execute(0, 0.2f, 1, amplitude);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = GameObject.Find("GameState").GetComponent<GameState>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
