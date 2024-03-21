85dc09d924c6b9c5d933cd108ec21803d25fc989
ferrisdavid <davidferris195@gmail.com>
2024-03-21 02:58:58 -0400

    Added hole detection for coursehole to enable ball fall through + win state

---
+++ ShortGameGolf/Assets/Scripts/HoleDetect.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Sits on The Golf Course Hole Inside Collder Object and Handles the Detection of the ball, triggers physics updates and success state.
+public class HoleDetect : MonoBehaviour
+{
+    // Reference to Global Game State to Trigger Win State.
+    [SerializeField]
+    private GameState gameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("ball")) {
+            other.gameObject.layer = 6;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("ball")) {
+            other.gameObject.layer = 3;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
