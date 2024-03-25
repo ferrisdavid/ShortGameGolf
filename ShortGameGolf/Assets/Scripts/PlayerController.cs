1772a0ce96ad8f00ec1259c70ab802c90e60614f
ferrisdavid <davidferris195@gmail.com>
2024-03-25 04:53:25 -0400

    Added enable/disable of the golf cart for travel interaction based on the teleportationEnabled state

--- ShortGameGolf/Assets/Scripts/PlayerController.cs
+++ ShortGameGolf/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject golfCart;
+
     public bool isTeleportEnabled = false;
 
     public void ToggleTeleportEnable(bool isEnabled) {
         isTeleportEnabled = isEnabled;
+
+        // Render the Golf Cart/Display Button for Teleportation
+        golfCart.SetActive(isEnabled);
     }
 
     // Start is called before the first frame update
