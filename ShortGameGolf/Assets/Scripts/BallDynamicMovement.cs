11882756b6a9edf0b0e47e17c4cc7e7c98370164
ferrisdavid <davidferris195@gmail.com>
2024-03-25 04:54:49 -0400

    Added golf cart + tweaked course hole particle system + fixed the ball hole interaction

--- ShortGameGolf/Assets/Scripts/BallDynamicMovement.cs
+++ ShortGameGolf/Assets/Scripts/BallDynamicMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BallDynamicMovement : MonoBehaviour
 {
+    // Initial Freeze State.
+    private bool isFrozen = true;
+
+    // Rigidbody Values.
     [SerializeField]
     private float baseDrag;
     [SerializeField]
@@ -21,23 +25,26 @@ public class BallDynamicMovement : MonoBehaviour
     {
         ballRB = GetComponent<Rigidbody>();
 
+        // Initialize the Balls Drag Physics
         ballRB.drag = baseDrag;
         ballRB.angularDrag = baseAngularDrag;
+
+        // Freeze the ball initially.
+        ballRB.constraints = RigidbodyConstraints.FreezePosition;
+
+        // TODO; Determine how to unfreeze the ball after the club comes in close contact with the ball.
     }
 
     private void FixedUpdate() {
         // Handle Dynamic Drag Forces to Prevent infinite rolling
-        if (ballRB.velocity.magnitude == 0) {
-            ballRB.drag = baseDrag;
+        if (ballRB.velocity.magnitude <= Mathf.Epsilon) {
             ballRB.angularDrag = baseAngularDrag;
         }
         else if (ballRB.velocity.magnitude <= 0.3) {
             ballRB.angularDrag = Mathf.Lerp(ballRB.angularDrag, 25, dragIncrement*100);
-            ballRB.drag = Mathf.Lerp(ballRB.drag, 25, dragIncrement*100);
         }
         else if (ballRB.velocity.magnitude <= 1) {
             ballRB.angularDrag = Mathf.Lerp(ballRB.angularDrag, 1, dragIncrement);
-            ballRB.drag = Mathf.Lerp(ballRB.drag, 1, dragIncrement);
         }
 
     }
