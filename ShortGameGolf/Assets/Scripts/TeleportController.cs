be0ede1fbcdeb2a3ecd1fd43b961ab7faeaa7fed
ferrisdavid <davidferris195@gmail.com>
2024-04-02 04:14:44 -0400

    temporarily removed the teleportation offset to teleport directly on top of ball

--- ShortGameGolf/Assets/Scripts/TeleportController.cs
+++ ShortGameGolf/Assets/Scripts/TeleportController.cs
@@ -19,7 +19,7 @@ public class TeleportController : MonoBehaviour
     // State Managers and Helpers.
     private PlayerController PlayerManager;
     [SerializeField]
-    private Vector3 teleportOffset;
+    private float teleportOffset;
     [SerializeField]
     private Transform ball;
 
@@ -27,13 +27,12 @@ public class TeleportController : MonoBehaviour
     void Start()
     {
         PlayerManager = GetComponent<PlayerController>();
-        // Teleport.AddOnStateDownListener(onTeleportRequest, handType);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void onTeleportRequest()
@@ -43,7 +42,7 @@ public class TeleportController : MonoBehaviour
             // Fade Screen to black on successful Teleport Request before moving
             SteamVR_Fade.View(Color.black, 1.0f);
             Vector3 difference = rig.transform.position - cam.transform.position;
-            Vector3 teleportPoint = ball.position - teleportOffset;
+            Vector3 teleportPoint = ball.position;
             // Start Fade Co routine which handles player movement and fade in
             StartCoroutine(TeleportFade(teleportPoint, difference));
         }
@@ -51,13 +50,13 @@ public class TeleportController : MonoBehaviour
 
     private void handleTeleport(Vector3 teleportPoint, Vector3 camPlayerDiff)
     {
-        // Stop the Ball.
-        ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
         // Teleport Player.
         rig.transform.position = new Vector3(camPlayerDiff.x + teleportPoint.x, ball.position.y, camPlayerDiff.z + teleportPoint.z);
         // Get Look Vector to Hole.
         Vector3 lookToHole = GameObject.Find("CourseHole").transform.position - rig.transform.position;
         lookToHole.y = 0.0f;
+
+        // Rotate Player to face the hole
         rig.transform.localRotation = Quaternion.LookRotation(lookToHole);
     }
 
