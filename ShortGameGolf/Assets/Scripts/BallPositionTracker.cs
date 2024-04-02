ee6c1e66dce97f7940d746e8d7f79360ced33dce
ferrisdavid <davidferris195@gmail.com>
2024-04-02 04:35:43 -0400

    Added trail to golf ball to make the ball more identifiable when hit

--- ShortGameGolf/Assets/Scripts/BallPositionTracker.cs
+++ ShortGameGolf/Assets/Scripts/BallPositionTracker.cs
@@ -6,14 +6,20 @@ public class BallPositionTracker : MonoBehaviour
 {
     // Track the Position of the Golf Ball and Player to determine when teleportation should be enabled/prompted
 
-
+    // Reference to Player Object
     [SerializeField]
     private GameObject player;
+
+    // Min Distance Threshold for Player to Ball to allow teleportation.
     [SerializeField]
     private float teleportEnableThreshold;
-
+    
+    // Reference to PlayerController
     private PlayerController PlayerManager;
 
+    // Reference to Ball Trail Particle System.
+    [SerializeField]
+    private ParticleSystem ballTrail;
 
 
     // Start is called before the first frame update
@@ -25,7 +31,14 @@ public class BallPositionTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GetComponent<Rigidbody>().velocity.sqrMagnitude > 0) {
+            ballTrail.Play();
+            ScaleBallTrailByVelocity();
+            AlignBallTrail();
+        }
+        else {
+            ResetTrailToDefault();
+        }
     }
 
     private void FixedUpdate() {
@@ -40,5 +53,25 @@ public class BallPositionTracker : MonoBehaviour
     // Helper Function to Compute the distance from the player to the ball used to determine whether teleportation should be enabled/prompted
     private float ComputeDistanceToPlayer(Transform player) {
         return (transform.position - player.transform.position).magnitude;
+    }
+
+    // Helper Function to Manage Particle System on Ball
+    private void AlignBallTrail() {
+        Vector3 ballVelocity = GetComponent<Rigidbody>().velocity;
+        ParticleSystem.ShapeModule systemShape = ballTrail.shape;
+
+        systemShape.rotation = Quaternion.FromToRotation(ballVelocity.normalized, systemShape.rotation).eulerAngles;
+    }
+
+    private void ScaleBallTrailByVelocity() {
+        Vector3 ballVelocity = GetComponent<Rigidbody>().velocity;
+        ParticleSystem.MainModule trailProperties = ballTrail.main;
+        trailProperties.startSpeed = Mathf.Clamp(ballVelocity.sqrMagnitude, 0, 20);
+    }
+
+    private void ResetTrailToDefault() {
+        ballTrail.Stop();
+        ParticleSystem.MainModule trailProperties = ballTrail.main;
+        trailProperties.startSpeed = 0;
     }
 }
