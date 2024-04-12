80a5b3c94b1ec4b7545ab96196ccf5d097e848e2
ferrisdavid <davidferris195@gmail.com>
2024-04-12 02:15:32 -0400

    Added accessible next hole and quit to main menu traversal event functions for use in buttons

--- ShortGameGolf/Assets/Scripts/PlayerController.cs
+++ ShortGameGolf/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private GameState gameState;
+
     [SerializeField]
     private GameObject golfCart;
 
@@ -13,20 +15,40 @@ public class PlayerController : MonoBehaviour
         isTeleportEnabled = isEnabled;
 
         // Render the Golf Cart/Display Button for Teleportation
-        golfCart.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        golfCart.transform.localPosition = new Vector3(golfCart.transform.localPosition.x, 1.5f, golfCart.transform.localPosition.z);
-        golfCart.SetActive(isEnabled);
+        if (!gameState.isWin) {
+            golfCart.transform.localRotation = Quaternion.Euler(Vector3.zero);
+            golfCart.transform.localPosition = new Vector3(golfCart.transform.localPosition.x, 1.5f, golfCart.transform.localPosition.z);
+            golfCart.SetActive(isEnabled);
+        }
+    }
+
+    public void goNextHole() {
+        if (gameState) gameState.LoadNextHole();
+    }
+
+    public void quitToMain() {
+        if (gameState) gameState.ReturnToClubhouse();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameState = GameObject.Find("GameState").GetComponent<GameState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Enable the Golf Cart Travel to Next Hole Interface.
+        if (gameState.isWin) {
+            if (!golfCart.activeSelf) {
+                golfCart.transform.localRotation = Quaternion.Euler(Vector3.zero);
+                golfCart.transform.localPosition = new Vector3(golfCart.transform.localPosition.x, 1.5f, golfCart.transform.localPosition.z);
+            }
+
+            golfCart.SetActive(true);            
+            golfCart.transform.Find("Button").gameObject.SetActive(false);
+            golfCart.transform.Find("NextHoleBtn").gameObject.SetActive(true);
+        }
     }
 }
